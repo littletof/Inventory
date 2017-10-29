957fa1b517ad248c1a6c25171cf9c03349a001f8
matefelegyhazy <mate.felegyhazy@gmail.com>
2017-10-29 18:31:40 +0100

    users oldal elkészítése
    Users fülön megjelennek a regisztrált felhasználók és email címük egy default képpel.

--- Mobil/app/tabs/users/UsersView.js
+++ Mobil/app/tabs/users/UsersView.js
@@ -1,8 +1,24 @@
-const UsersViewModel = require("./users-view-model");
+var dialogsModule = require("ui/dialogs");
+var observableModule = require("data/observable")
+var ObservableArray = require("data/observable-array").ObservableArray;
 
-function onLoaded(args) {
-    const component = args.object;
-    component.bindingContext = new UsersViewModel();
-}
+var page;
 
-exports.onLoaded = onLoaded;
+var UsersViewModel = require("./users-view-model");
+var users = new UsersViewModel([]);
+
+var pageData = new observableModule.fromObject({
+    users: users
+});
+
+exports.onLoaded = function(args) {
+    page = args.object;
+    page.bindingContext = pageData;
+
+    users.empty();
+    users.load();
+};
+exports.onTap = function (args) {
+    var index = args.index;
+    alert("Navigating to details is under construction :(");
+};
--- Mobil/app/tabs/users/users-view-model.js
+++ Mobil/app/tabs/users/users-view-model.js
@@ -1,11 +1,76 @@
-const observableModule = require("data/observable");
+var ObservableArray = require("data/observable-array").ObservableArray;
+var config = require("../../shared/config");
+var firebase = require("nativescript-plugin-firebase");
 
-function UsersViewModel() {
-    const viewModel = observableModule.fromObject({
-
+function indexOf(item) {
+    var match = -1;
+    this.forEach(function(loopItem, index) {
+      if (loopItem.id === item.key) {
+        match = index;
+      }
     });
+    return match;
+  }
+
+function Users(items) {
+    var viewModel = new ObservableArray(items);
+    
+    viewModel.indexOf = indexOf;
+    
+    viewModel.load = function() {
+        var onChildEvent = function(result) {
+            var matches = [];
+            
+            if (result.type === "ChildAdded") {
+                if (result.value.UID === config.uid) {
+                    viewModel.push({
+                        name: result.value.name,
+                        email: result.value.email_address,
+                        role: result.value.role,
+                        id: result.key
+                    });
+                }
+            } else if (result.type === "ChildRemoved") {
+                matches.push(result);
+                matches.forEach(function(match) {
+                    var index = viewModel.indexOf(match);
+                    viewModel.splice(index, 1);
+                });
+            }
+        };
+
+        return firebase.addChildEventListener(onChildEvent, "/users").then(
+            function() {
+                console.log("firebase.addChildEventListener added");
+            },
+            function(error) {
+                console.log("firebase.addChildEventListener error: " + error);
+            }
+        )
+    };
+
+    viewModel.add = function(user) {
+        return firebase.push( '/users', {
+            'email_address': user.get("email"),
+            'name': user.get("name"),
+            'role': user.get("role")
+        });
+      };
+    
+    viewModel.delete = function(index) {
+        var id = viewModel.getItem(index).id;
+        return firebase.remove("/users/"+id+"");
+    };
+    
+    viewModel.empty = function() {
+        while (viewModel.length) {
+            viewModel.pop();
+        }
+    };
 
     return viewModel;
 }
 
-module.exports = UsersViewModel;
+module.exports = Users;
+
+
