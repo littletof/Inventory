257ab79fe95b0c38316d91344428caedcc25230c
matefelegyhazy <mate.felegyhazy@gmail.com>
2017-10-30 02:29:00 +0100

    kölcsönzések elkészítése
    Kölcsönzések megjelennek a listában.

--- Mobil/app/tabs/lendings/LendingsView.js
+++ Mobil/app/tabs/lendings/LendingsView.js
@@ -1,8 +1,25 @@
-const LendingsViewModel = require("./lendings-view-model");
+var dialogsModule = require("ui/dialogs");
+var observableModule = require("data/observable")
+var ObservableArray = require("data/observable-array").ObservableArray;
 
-function onLoaded(args) {
-    const component = args.object;
-    component.bindingContext = new LendingsViewModel();
+var page;
+
+var LendingsViewModel = require("./lendings-view-model");
+var lendings = new LendingsViewModel([]);
+
+var pageData = new observableModule.fromObject({
+    lendings: lendings
+});
+
+exports.onLoaded = function onLoaded(args) {
+    page = args.object;
+    page.bindingContext = pageData;
+
+    lendings.empty();
+    lendings.load();
 }
 
-exports.onLoaded = onLoaded;
+exports.onTap = function (args) {
+    var index = args.index;
+    alert("Navigating to details is under construction :(");
+};
--- Mobil/app/tabs/lendings/lendings-view-model.js
+++ Mobil/app/tabs/lendings/lendings-view-model.js
@@ -1,11 +1,89 @@
-const observableModule = require("data/observable");
+var ObservableArray = require("data/observable-array").ObservableArray;
+var config = require("../../shared/config");
+var firebase = require("nativescript-plugin-firebase");
 
-function LendingsViewModel() {
-    const viewModel = observableModule.fromObject({
 
+function indexOf(item) {
+    var match = -1;
+    this.forEach(function(loopItem, index) {
+      if (loopItem.id === item.key) {
+        match = index;
+      }
     });
+    return match;
+  }
+
+function Lendings(items) {
+    var viewModel = new ObservableArray(items);
+    
+    viewModel.indexOf = indexOf;
+    
+    viewModel.load = function() {
+        var onChildEvent = function(result) {
+            var matches = [];
+            var borrowing =result.value;
+            
+            if (result.type === "ChildAdded") {
+                if(borrowing.UID == config.uid) {        
+                    var findDeviceName = function(device) {
+                        for(let uid in device.value) {  
+                            if(uid == borrowing.device_id) {
+                                var a = new Date(borrowing.start_date);
+                                var months = ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec'];
+                                var month0 = months[a.getMonth()];
+                                var date0 = a.getDate();
+                                    
+                                var b = new Date(borrowing.end_date);
+                                var month1 = months[b.getMonth()];
+                                var date1 = b.getDate();
+                    
+                                viewModel.push({
+                                    deviceName:device.value[uid].name,
+                                    interval:month0+"."+date0+" - "+month1+"."+date1,
+                                    quantity:"quantity borrowed:"+borrowing.device_number
+                                });
+                            }
+                        }
+                    }
+        
+                    firebase.query(
+                       findDeviceName,
+                        "/devices", 
+                        {
+                            singleEvent: true,
+                            orderBy: {
+                                type: firebase.QueryOrderByType.CHILD,
+                                value: 'name'
+                            }
+                        }
+                    );            
+                }
+            } else if (result.type === "ChildRemoved") {
+                matches.push(result);
+                matches.forEach(function(match) {
+                    var index = viewModel.indexOf(match);
+                    viewModel.splice(index, 1);
+                    });
+            }            
+        }
+        
+        return firebase.addChildEventListener(onChildEvent, "/lendings/present_lendings").then(
+            function() {
+                console.log("firebase.addChildEventListener added");
+            },
+            function(error) {
+                console.log("firebase.addChildEventListener error: " + error);
+            }
+        )
+    };
+    
+    viewModel.empty = function() {
+        while (viewModel.length) {
+            viewModel.pop();
+        }
+    };
 
     return viewModel;
 }
 
-module.exports = LendingsViewModel;
+module.exports = Lendings;
