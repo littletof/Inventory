2857b01fa3f4ec7987f0505aa9f930f7bb164ef3
Szalay Kristóf <szalay.kristof@upcmail.hu>
2017-11-05 21:47:06 +0100

    Renamed device_number to device_quantity in lendings

--- Web/src/app/lend-entry.ts
+++ Web/src/app/lend-entry.ts
@@ -3,28 +3,28 @@ export class LendEntry {
     device_id: string;
     start_date: Date;
     end_date: Date;
-    device_number: number;
+    device_quantity: number;
     comment: string;
 
 
-    constructor(user_id, device_id, start_date: Date, end_date: Date, device_number, comment){
+    constructor(user_id, device_id, start_date: Date, end_date: Date, device_quantity, comment){
         this.user_id = user_id;
         this.device_id = device_id;
         this.start_date = start_date;
         this.end_date = end_date;
-        this.device_number = device_number;
+        this.device_quantity = device_quantity;
         this.comment = comment;
     }
 
-    static from(user_id:string, device_id:string, start_date:string, end_date:string, device_number:number, comment:string): LendEntry{
+    static from(user_id:string, device_id:string, start_date:string, end_date:string, device_quantity:number, comment:string): LendEntry{
         /*this.user_id = user_id;
         this.device_id = device_id;
         this.start_date = new Date(start_date);
         this.end_date = new Date(end_date);
-        this.device_number = device_number;
+        this.device_quantity = device_quantity;
         this.comment = comment;
         */
-        return new LendEntry(user_id, device_id,new Date(start_date),  new Date(end_date), device_number, comment);
+        return new LendEntry(user_id, device_id,new Date(start_date),  new Date(end_date), device_quantity, comment);
     }
 
     static fromJSON(js): LendEntry{
@@ -32,16 +32,16 @@ export class LendEntry {
         this.device_id = device_id;
         this.start_date = new Date(start_date);
         this.end_date = new Date(end_date);
-        this.device_number = device_number;
+        this.device_quantity = device_quantity;
         this.comment = comment;
         */
-        return new LendEntry(js.user_id, js.device_id,new Date(js.start_date),  new Date(js.end_date), js.device_number, js.comment);
+        return new LendEntry(js.user_id, js.device_id,new Date(js.start_date),  new Date(js.end_date), js.device_quantity, js.comment);
     }
 
     static fromtoJSON(snap){
         let js = snap.payload.val();
         return {key: snap.payload.key, user_id: js.user_id, device_id: js.device_id, start_date: new Date(js.start_date),
-            end_date: new Date(js.end_date), device_number: js.device_number, comment: js.comment};
+            end_date: new Date(js.end_date), device_quantity: js.device_quantity, comment: js.comment};
     }
 
     getJSON(): any{
@@ -50,7 +50,7 @@ export class LendEntry {
             device_id: this.device_id,
             start_date: this.start_date.getTime(),
             end_date: this.end_date.getTime(),
-            device_number: this.device_number,
+            device_quantity: this.device_quantity,
             comment: this.comment
 
         };
