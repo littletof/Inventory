c911fc7cd2db2c181eb7a64d9e585eaff7e3084e
vadlevente <vad.levente@gmail.com>
2017-11-13 11:32:56 +0100

    eszközöknél képek megjelenítése
    majd keresek hozzá valami image cache plugint, amiket eddig próbáltam teljesen haszontalanok voltak

--- Mobil/app/app.js
+++ Mobil/app/app.js
@@ -5,13 +5,7 @@ require('nativescript-i18n');
 
 const application = require("application");
 
-var imageCache = require("nativescript-web-image-cache");
 
-if (application.android) {
-    application.on("launch", function () {
-        imageCache.initialize();
-    });
-}
 
 var firebase = require("nativescript-plugin-firebase");
 
