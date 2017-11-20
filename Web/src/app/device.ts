12959f430e0a896d5de23c3c24fee337934138b6
Szalay Kristóf <szalay.kristof@upcmail.hu>
2017-11-20 11:01:34 +0100

    Device editing fixed: quantity_available, image

--- Web/src/app/device.ts
+++ Web/src/app/device.ts
@@ -5,13 +5,15 @@ export class Device {
   quantity_available: number;
   quantity_total: number;
   tags = {};
+  image : string;
 
-  constructor(_name: string, _qt: number, _desc: string, _tags: {}){
+  constructor(_name: string, _qt: number, _desc: string, _tags: {}, _img: string){
     this.name = _name;
     this.quantity_available = _qt;
     this.quantity_total = _qt;
     this.description = _desc;
     this.tags = _tags;
+    this.image = _img;
   }
 
   static fromJSON(snap): any{
