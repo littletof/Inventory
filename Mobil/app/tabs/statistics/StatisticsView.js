fcd1e6d9c47bc7c852946d37d677d5dd25e60758
vadlevente <vad.levente@gmail.com>
2017-12-03 18:17:33 +0100

    RIP statisztikák

--- Mobil/app/tabs/statistics/StatisticsView.js
+++
@@ -1,8 +0,0 @@
-const StatisticsViewModel = require("./statistics-view-model");
-
-function onLoaded(args) {
-    const component = args.object;
-    component.bindingContext = new StatisticsViewModel();
-}
-
-exports.onLoaded = onLoaded;
