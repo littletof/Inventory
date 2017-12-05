f68e9660fc2b18f2d26d1fca1d226d77fd237ce0
Szalay Kristóf <szalay.kristof@upcmail.hu>
2017-12-05 17:01:59 +0100

    IMEI addable editable stb... maybe DONE?!

--- Web/src/app/device-edit-dialog/device-edit-dialog.component.ts
+++ Web/src/app/device-edit-dialog/device-edit-dialog.component.ts
@@ -1,9 +1,9 @@
 import {Component, Inject, OnInit, ViewChild} from '@angular/core';
-import {MAT_DIALOG_DATA, MatChipInputEvent, MatDialogRef} from "@angular/material";
+import {MAT_DIALOG_DATA, MatChipInputEvent, MatDialogRef, MatStepper} from "@angular/material";
 import { SPACE} from "@angular/cdk/keycodes";
 import {Device} from "../device";
 import {DatabaseService} from "../backend-services/database.service";
-import {NgForm} from "@angular/forms";
+import {FormBuilder, FormControl, FormGroup, NgForm} from "@angular/forms";
 
 @Component({
   selector: 'app-device-edit-dialog',
@@ -40,7 +40,13 @@ export class DeviceEditDialogComponent implements OnInit {
     deleteImage:boolean;
 
 
-    constructor(@Inject(MAT_DIALOG_DATA) public data: any, public dialogRef: MatDialogRef<DeviceEditDialogComponent>, public db: DatabaseService) {
+    isLinear = true;
+    firstFormGroup: FormGroup;
+    secondFormGroup: FormGroup;
+
+    imeiToBE: any[] = [];
+
+    constructor(@Inject(MAT_DIALOG_DATA) public data: any, public dialogRef: MatDialogRef<DeviceEditDialogComponent>,private _formBuilder: FormBuilder, public db: DatabaseService) {
         this.time = new Date().getTime().toString();
 
 
@@ -48,13 +54,82 @@ export class DeviceEditDialogComponent implements OnInit {
             this.loadDevice(data);
             this.editing = true;
         }else {
-            this.device_quantity = this.default_device_quantity;
+            this.device_quantity = 1;//this.default_device_quantity;
+            this.imeiToBE = [{value:null}]
         }
 
 
     }
 
     ngOnInit() {
+
+        this.firstFormGroup = this._formBuilder.group({
+            firstCtrl: ['']
+        });
+
+        //this.firstFormGroup.setErrors({'not good': true});
+
+        this.secondFormGroup = this._formBuilder.group({
+            secondCtrl: ['']
+        });
+
+        //this.secondFormGroup.setErrors({init: true});
+
+    }
+
+    step(stepper: MatStepper){
+        switch(stepper.selectedIndex){
+            case 0:
+                this.isFirstValid();
+                break;
+            case 1:
+                //this.isSecondValid();
+                break;
+        }
+        stepper.next();
+    }
+
+    stepback(stepper: MatStepper){
+        stepper.previous();
+    }
+
+    isFirstValid(){
+        this.firstFormGroup.setErrors(null);
+        if(this.device_name == null || this.device_name.length == 0){
+            this.firstFormGroup.setErrors({nameempty:true});
+            return false;
+        }
+        return true;
+    }
+
+    isSecondValid(){
+        this.secondFormGroup.setErrors(null);
+        for(let i in this.imeiToBE){
+            if(this.imeiToBE[i].value == null || this.imeiToBE[i].value == ""){
+                this.secondFormGroup.setErrors({i: true});
+                return false;
+            }
+        }
+        return true;
+
+    }
+
+    canRemove(index):boolean{
+
+        if(this.editing && this.imei[this.imeiToBE[index].value]){
+            return this.imei[this.imeiToBE[index].value].available;
+        }
+        return true;
+    }
+
+    addIMEI(){
+        this.imeiToBE.push({value:""});
+        this.device_quantity+=1;
+    }
+
+    removeIMEI(index){
+        this.imeiToBE.splice(index, 1);
+        this.device_quantity-=1;
     }
 
     loadDevice(device){
@@ -73,19 +148,30 @@ export class DeviceEditDialogComponent implements OnInit {
 
         this.imei = device.imei;
 
+        this.loadIMEIS();
+
+    }
+
+    loadIMEIS(){
+        for(let i in this.imei){
+            this.imeiToBE.push({value:i});
+        }
     }
 
 
 
     isValid(): boolean{
-        if(this.device_quantity>0 && this.device_name.trim() != ""){
+        if(this.isFirstValid() && this.isSecondValid()){
             return true;
         }
+        console.log("invalid");
         return false;
     }
 
     addDevice(f: NgForm){
         if(f.valid && this.isValid()) {
+
+
 
             let device = this.prepareDevice();
 
@@ -132,10 +218,37 @@ export class DeviceEditDialogComponent implements OnInit {
         this.tags.forEach(t => {
             this.device_tags[t.name] = true;
         });
-        //TODO to be able to add imeis
+        this.prepareIMEIS();
         return new Device(this.device_name, this.device_quantity, this.device_description, this.device_tags, this.device_image,this.imei);
     }
 
+    prepareIMEIS(){
+        if(this.editing){
+
+            let prevIMEI = this.imei;
+            this.imei = {};
+
+            for(let i in this.imeiToBE){
+                let aimei = this.imeiToBE[i].value;
+                if (typeof(prevIMEI[aimei]) == 'undefined'){
+                    this.imei[aimei] = {available: true, comments: ""};
+                }else{
+                    this.imei[aimei] = prevIMEI[aimei];
+                }
+            }
+
+            this.device_quantity = this.imeiToBE.length;
+
+
+        }else{
+            for(let i in this.imeiToBE){
+                let newIMEI = this.imeiToBE[i].value;
+                this.imei[newIMEI] = {available:true};
+            }
+            this.device_quantity = this.imeiToBE.length;
+
+        }
+    }
 
 
 
